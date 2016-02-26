910a77892059fbea5a3166f4869d4188899c0cad
Justin Inouye <inouju@gmail.com>
2016-02-25 18:30:06 -0800

    Add blank activities

---
+++ app/src/main/java/edu/uw/jyinouye/assassin/ShopActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.jyinouye.assassin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
